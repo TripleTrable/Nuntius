ba725d3c3ec4a92bc393dbb85f3f68fde52d1034
Fynn Michell-Auli <f.michell-auli@live.de>
2020-01-19 23:48:13 +0100

    Add the user to the linked list

--- nuntiusServer/UserController.cs
+++ nuntiusServer/UserController.cs
@@ -19,26 +19,7 @@ namespace NuntiusServer
 
 			loggedinUser.AddLast(user);
 
-			return GenerateToken();
-		}
-		
-		/// <summary>
-		/// Grenrate a Token
-		/// </summary>
-		private static string GenerateToken(int length = 32)
-		{
-			string chars = "ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789@€%!#§$&";
-			char[] token = new char[length];
-			var random = new Random();
-
-			for (int i = 0; i < length; i++)
-			{
-				token[i] = chars[random.Next(chars.Length)];
-			}
-
-			//ToDo: Check if the token ist used
-
-			return new String(token);
+			return "";
 		}
 
 	}
