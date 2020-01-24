6dd5017f3af41e65b8868a616da83728bf7c296a
Arthur Finkelmann <arthfink@gmail.com>
2020-01-24 16:09:37 +0100

    Login Reqeust is send with the input of the user

--- nuntiusClientChat/nuntiusClientChat/MainPage.xaml.cs
+++ nuntiusClientChat/nuntiusClientChat/MainPage.xaml.cs
@@ -27,7 +27,7 @@ namespace nuntiusClientChat
         }
 
 
-        public void ChatGridLayout()
+        public void ChatPage()
         {
             Grid grid = new Grid
             {
@@ -73,51 +73,17 @@ namespace nuntiusClientChat
         }
         public void LoginPage()
         {
-            Grid grid = new Grid
-            {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                BackgroundColor = Color.Gray,
-
-                RowDefinitions =
-                {
-                    new RowDefinition { Height = new GridLength(250, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
-                    new RowDefinition { Height = new GridLength(10, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
-                    new RowDefinition { Height = new GridLength(150, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
-                    new RowDefinition { Height = new GridLength(100, GridUnitType.Absolute) },
-                    new RowDefinition { Height = new GridLength(1,GridUnitType.Star)},
-                    new  RowDefinition{ Height = new GridLength(1,GridUnitType.Star)},
-                    new RowDefinition { Height = new GridLength (20, GridUnitType.Absolute)},
-
-                },
-                ColumnDefinitions =
-                {
-                    new ColumnDefinition { Width = new GridLength(50, GridUnitType.Absolute) },
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
-                    new ColumnDefinition { Width = new GridLength(50, GridUnitType.Absolute) }
-                }
-            };
-            Grid pwdGrid = new Grid
-            {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-
-                ColumnDefinitions =
-                {
-                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
-                    new ColumnDefinition { Width = new GridLength(50, GridUnitType.Absolute) },
-                }
-
-            };
+            Grid grid, pwdGrid;
+            UserCredentialsMask(out grid, out pwdGrid);
 
             aliasEntry = new Entry();
 
-            aliasEntry.MaxLength = 256;
+            aliasEntry.MaxLength = 32;
 
             pwdEntry = new Entry();
 
-            pwdEntry.IsPassword = true;        
+            pwdEntry.IsPassword = true;
+            pwdEntry.MaxLength = 32;
 
             Button loginButton = new Button();
 
@@ -127,11 +93,13 @@ namespace nuntiusClientChat
             Button registerButton = new Button();
             Label registerLabel = new Label();
 
+            registerButton.Clicked += RegisterButton_Clicked_;
+
             ImageButton showPwd = new ImageButton { Source = @"C:\!nuntiusChat\iah71-messenger-nuntius\nuntiusClientChat\nuntiusClientChat\Show.png", VerticalOptions = LayoutOptions.FillAndExpand };
             showPwd.Clicked += ShowPwd_Clicked;
             //showPwd.Source = @"C:\!nuntiusChat\iah71-messenger-nuntius\nuntiusClientChat\nuntiusClientChat\Show.png";
 
-            registerButton.Opacity = 0.1;
+            registerButton.Opacity = 0;
             registerLabel.Text = "Regesteriren";
             registerLabel.HorizontalTextAlignment = TextAlignment.Center;
             registerLabel.VerticalTextAlignment = TextAlignment.Center;
@@ -153,6 +121,90 @@ namespace nuntiusClientChat
 
             this.Content = grid;
         }
+
+        private void RegisterButton_Clicked_(object sender, EventArgs e)
+        {
+            RegisterPage();
+        }
+
+        public void RegisterPage()
+        {
+            Grid grid, pwdGrid, backGrid;
+            UserCredentialsMask(out grid, out pwdGrid);
+
+            backGrid = new Grid
+            {
+                RowDefinitions =
+                {
+                     new RowDefinition { Height = new GridLength( 1, GridUnitType.Star)},
+                     new RowDefinition { Height = new GridLength(190, GridUnitType.Absolute) },
+                }
+
+            };
+
+            aliasEntry = new Entry();
+
+            aliasEntry.MaxLength = 32;
+            aliasEntry.Placeholder = "Benutzer Name";
+
+            pwdEntry = new Entry();
+
+            pwdEntry.IsPassword = true;
+            pwdEntry.MaxLength = 32;
+            pwdEntry.Placeholder = "Passwort";
+
+            Button registerButton = new Button();
+            registerButton.BorderColor = Color.Red;
+
+            Button backButton = new Button();
+            backButton.Clicked += BackButton_Clicked;
+
+            registerButton.Text = "Regestrieren";
+            registerButton.Clicked += RegisterButton_Clicked;
+
+            ImageButton showPwd = new ImageButton { Source = @"C:\!nuntiusChat\iah71-messenger-nuntius\nuntiusClientChat\nuntiusClientChat\Show.png", VerticalOptions = LayoutOptions.FillAndExpand };
+            showPwd.Clicked += ShowPwd_Clicked;
+
+            grid.Children.Add(aliasEntry, 1, 2, 1, 2);
+            grid.Children.Add(pwdEntry, 1, 2, 3, 4);
+            grid.Children.Add(pwdGrid, 1, 2, 3, 4);
+            pwdGrid.Children.Add(pwdEntry, 0, 1, 0, 1);
+            pwdGrid.Children.Add(showPwd, 1, 2, 0, 1);
+
+            //Grid to get back to Login
+            grid.Children.Add(backGrid, 0, 1, 0, 1);
+            backGrid.Children.Add(backButton, 0, 1, 0, 1);
+           
+
+            grid.Children.Add(registerButton, 1, 2, 7, 8);
+
+            this.Content = grid;
+        }
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+            LoginPage();
+        }
+
+        private async void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+            if (pwdEntry.Text == null || aliasEntry.Text == null)
+            {
+                return;
+            }
+            else
+            {
+                //TODO: Loding icon until the User has a token from the Server 
+                await NetworkController.SendRegisterRequest(aliasEntry.Text, pwdEntry.Text);
+                aliasEntry.Text = null;
+                pwdEntry.Text = null;
+            }
+
+        }
+
+       
+
+  
 
         private async void LoginButton_Clicked(object sender, EventArgs e)
         {
@@ -179,6 +231,47 @@ namespace nuntiusClientChat
                 pwdEntry.IsPassword = true;
 
             //pwdEntry.IsPassword ? pwdEntry.IsPassword = false : pwdEntry.IsPassword = true;
+        }
+
+        private static void UserCredentialsMask(out Grid grid, out Grid pwdGrid)
+        {
+            grid = new Grid
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.Gray,
+
+                RowDefinitions =
+                {
+                    new RowDefinition { Height = new GridLength(250, GridUnitType.Absolute) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(10, GridUnitType.Absolute) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(150, GridUnitType.Absolute) },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = new GridLength(100, GridUnitType.Absolute) },
+                    new RowDefinition { Height = new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{ Height = new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition { Height = new GridLength (20, GridUnitType.Absolute)},
+
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(50, GridUnitType.Absolute) },
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(50, GridUnitType.Absolute) }
+                }
+            };
+            pwdGrid = new Grid
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(50, GridUnitType.Absolute) },
+                }
+
+            };
         }
 
     }
