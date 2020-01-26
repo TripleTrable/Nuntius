590216728b1c40cbbc92d4e9f35958dde3c0de4b
Fynn Michell-Auli <f.michell-auli@live.de>
2020-01-26 18:31:49 +0100

    Add Odbc connection

--- nuntiusServer/DbController.cs
+++ nuntiusServer/DbController.cs
@@ -1,12 +1,16 @@
 using System;
 using nuntiusModel;
+using System.Data.Odbc;
 
 namespace NuntiusServer
 {
 	//ToDo: implement
 	public static class DbController
 	{
+		private static string connectionString = "Driver={PostgreSQL};Server=localhost;Port=5432;Database=nuntius;Uid=fynn;";
+
 		/// <summary>
+		/// temporörer platzhalter
 		/// </summary>
 		public static bool CheckToken(string token)
 		{
@@ -42,6 +46,38 @@ namespace NuntiusServer
 		/// </summary>
 		public static void AssignToken(string alias, string token)
 		{
+
+		}
+
+		/// <summary>
+		///  Connect to the database and execute a non querry
+		/// </summary>
+		private static void ExecuteNonQuerry(OdbcCommand command)
+		{
+			int result;
+
+			// create SqlConnection object
+			using (OdbcConnection con = new OdbcConnection(connectionString))
+			{
+				try
+				{
+					// open connection to database
+					con.Open();
+					command.Connection = con;
+
+					//Execute the commmand
+					result = command.ExecuteNonQuery();
+				}
+				catch (Exception e)
+				{
+					throw e;
+				}
+				finally
+				{
+					// close connection to database
+					con.Close();
+				}
+			}
 
 		}
 	}
